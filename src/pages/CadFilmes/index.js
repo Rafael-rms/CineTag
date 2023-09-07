b7b7c20f637280835261836d11cbc3bcf12f30a6
Rafael-rms <marquesrafael3@hotmail.com>
2023-09-06 21:08:58 -0300

    primeiro commit

---
+++ src/pages/CadFilmes/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "components/Banner";
+
+function CadFilmes(){
+    return(
+        <>
+        {/* <Banner/> */}
+        </>
+    )
+}
+
+export default CadFilmes;
