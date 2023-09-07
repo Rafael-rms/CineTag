14b44e03df4fd9dca1144e63d6a07e16fc12e029
Rafael-rms <marquesrafael3@hotmail.com>
2023-09-06 21:32:44 -0300

    Atualização da tela Cadastrar filmes

--- src/pages/CadFilmes/index.js
+++ src/pages/CadFilmes/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Banner from "components/Banner";
+import Titulo from "components/Titulo";
+import CampoTexto from "components/CampoTexto";
 
 function CadFilmes(){
     return(
         <>
-        {/* <Banner/> */}
+        <Banner imagem='favoritos'/>
+        <Titulo>
+            <h1>Cadastro de filmes</h1>
+        </Titulo>
+        <CampoTexto placeholder="Inserir Nome"/>
+        <CampoTexto placeholder="Inserir Gênero"/>
+        <CampoTexto placeholder="Inseir Imagem"/>
         </>
     )
 }
